d3f43d861bd043c1c6ae287e7a2aba31ce87895b
Tharcila Contursi <tharcilakcontursi@gmail.com>
2024-03-06 15:32:44 +0100

    commit exercicio ajax

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const nameElement = document.querySelector('#meunome');
+    const userElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#linkGitHub');
 
-$(document).ready(function(){
-
-    $('form').on('submit', function(e){
-        e.preventDefault();
-        const inputNomeDaTarefa = $('#nome-tarefa').val();
-        const addNaLista = $('#lista-tarefa');
-        
-        $('<li>').text(inputNomeDaTarefa).appendTo(addNaLista) ;
+    fetch('https://api.github.com/users/tharcilacontursi')
+    .then(function(res){
+        return res.json();
     })
-    
-    $('#lista-tarefa').on('click','li', function (){
-    $(this).toggleClass('item-completado');
-    })
-
-})
+    .then(function(json){
+        nameElement.innerText = json.name;
+        userElement.innerText = json.login;
+        avatarElement.src = json.avatar_url;
+        reposElement.innerText = json.public_repos;
+        followersElement.innerText = json.followers;
+        followingElement.innerText = json.following;
+        linkElement.href = json.html_url;
+    });
+});
