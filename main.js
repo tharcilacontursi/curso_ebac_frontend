013b40a409a909ebb92cdb0927007ba296944704
Tharcila Contursi <tharcilakcontursi@gmail.com>
2024-03-07 17:15:44 +0100

    exercicio sobre orientacao a objeto com js

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
+function Produto (nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
 
-$(document).ready(function(){
+function ProdutoEletronico (nome, preco, marca){
+    Produto.call(this, nome, preco);
+    this.marca = marca;
 
-    $('form').on('submit', function(e){
-        e.preventDefault();
-        const inputNomeDaTarefa = $('#nome-tarefa').val();
-        const addNaLista = $('#lista-tarefa');
-        
-        $('<li>').text(inputNomeDaTarefa).appendTo(addNaLista) ;
-    })
     
-    $('#lista-tarefa').on('click','li', function (){
-    $(this).toggleClass('item-completado');
-    })
+}
 
-})
+function ProdutoAlimenticio (nome, preco, marca, validade){
+    Produto.call(this, nome, preco);
+    this.marca = marca;
+    this.validade = validade;
+
+}
+
+const eletronico1 = new ProdutoEletronico ("celular", 5000 , "samsung");
+const eletronico2 = new ProdutoEletronico ("televisao", 10000, "LG");
+const alimento1 = new ProdutoAlimenticio ("bala",3 , "Fini", "10/2028");
+const alimento2 = new ProdutoAlimenticio ("chocolate", 8, "Garoto", "9/2030");
+
+console.log(eletronico1);
+console.log(eletronico2);
+
+
+console.log(alimento1);
+console.log(alimento2);
+
+
+
